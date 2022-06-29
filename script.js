f0017b27e73ba076d6f977ad23129f5e55d3ab2e
Artur Poffo <arturpoffoc@hotmail.com>
2022-06-29 14:37:05 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// slide
 let img = document.getElementById("image-home");
 let radio1 = document.getElementById("radio1");
 let radio2 = document.getElementById("radio2");
@@ -17,4 +18,23 @@ function slide() {
     circle.style.backgroundImage = "url(./assets/home3.svg)"
   }
 }
+//fim slide
 
+// menu mobile
+let menu = document.getElementsByClassName('menu-mobile')[0]
+let mobilelist = document.getElementsByClassName('mobile-list')[0]
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('change')
+  mobilelist.classList.toggle('open')
+})
+//fim menu mobile
+
+//fechar menu
+let links = document.querySelectorAll('.navlink')
+
+function removemenu() {
+  menu.classList.remove('change')
+  mobilelist.classList.remove('open')
+}
+//fim fechar menu
